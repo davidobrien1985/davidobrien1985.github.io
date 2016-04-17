cefe5f1257154ff9fb9a31081640befacf0b648b
David O'Brien <me@david-obrien.net>
2016-04-17 11:04:38 +1000

    initial commit

---
+++ replace_strings_in_files.ps1
@@ -0,0 +1,12 @@
+﻿[cmdletBinding()]
+param (
+  [string]$oldvalue,
+  [string]$newvalue,
+  [string]$path_to_files = 'C:\githubpages\_posts'
+)
+
+$files = Get-ChildItem -Path $path_to_files -Recurse
+
+foreach ($file in $files) {
+  (Get-Content -Path $file.FullName).Replace("$oldvalue","$newvalue") | Set-Content -Path $file.FullName
+}
